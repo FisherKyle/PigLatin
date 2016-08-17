136bce74314f1cb94f835fe88567557aad61bc23
Kyle Fisher and Peter Armington <peter@>
2016-08-17 14:26:57 -0400

    establish vowel array to test over, append ay to all strings

--- js/styles.js
+++ js/styles.js
@@ -13,6 +13,18 @@ $(document).ready(function() {
 // Backsies
 
   function translate(userSentence) {
-    var pigglySentence = (userSentence + "ay")
+
+    var vowels = ['a','e','i','o','u','A','E','I','O','U']; // creating an array of vowels called vowels
+    var pigglyArray = userSentence.split(''); // taking the input (userSentence in this case) and performing the split method on it. results are assigned to pigglyArray.
+
+    //remove first letter if a vowels
+    if(vowels.includes(pigglyArray[0])){
+      pigglyArray.pop();
+    }
+    pigglyArray.push('a');
+    pigglyArray.push('y');
+
+    var pigglySentence = pigglyArray.join(''); //
+
     return pigglySentence;
   }
